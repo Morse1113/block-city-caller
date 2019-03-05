2d79ac1ed5a7b20f45529915f80348d44975a934
Morse <1185585454@qq.com>
2019-03-05 16:38:23 +0800

    fix

--- main.js
+++ main.js
@@ -6,6 +6,6 @@ function callAuth() {
 function callContract() {
 	const player = {"id":"DefaultId","name":"liuwen","fatigue":100,"spirit":100,"gold":100,"power":100,"agility":100,"intelligence":100,"goodness":40,"buffSet":{},"achievements":{}}
 	const character = BlockCityGenesis.playerToCharacter(player);
-	console.log(character);
-	BlockCityGenesis.insertCharacter(character); 
+	const result = BlockCityGenesis.insertCharacter(character);
+	document.getElementById('doc-div').innerHTML=result;
 }
